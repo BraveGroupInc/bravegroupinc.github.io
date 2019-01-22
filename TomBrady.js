1acbffdf7e41d26d7c615a37bf2a10ba5d45ea3a
bravegroupco <45539720+bravegroupco@users.noreply.github.com>
2019-01-21 22:15:39 -0500

    TaDaa!

---
+++ TomBrady.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$("#Facebook").mouseenter(function(){
+    $("#Facebook").attr("src", "SocialMedia-02.jpg");
+    });
+$("#Facebook").mouseleave(function(){
+    $("#Facebook").attr("src", "SocialMedia-01.jpg");
+    });
+$("#Twitter").mouseenter(function(){
+    $("#Twitter").attr("src", "SocialMedia-06.jpg");
+    });
+$("#Twitter").mouseleave(function(){
+    $("#Twitter").attr("src", "SocialMedia-05.jpg");
+    });
+$("#Instagram").mouseenter(function(){
+    $("#Instagram").attr("src", "SocialMedia-04.jpg");
+    });
+$("#Instagram").mouseleave(function(){
+    $("#Instagram").attr("src", "SocialMedia-03.jpg");
+    });
+});
